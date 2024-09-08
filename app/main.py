bc113e85f4a83778a5f7d06a4104a38c31c460d4
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-09-08 18:03:16 +0800

    chore: return a response

--- app/main.py
+++ app/main.py
@@ -8,7 +8,17 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, client_address =   server_socket.accept() # wait for client
+    request = client_socket.recv(1024).decode("utf-8")
+    # request_line = request.split("\r\n") # which is the first line fo the http request
+    # method, path, protocol = request_line[0].split(" ") # split the first line into method, path and protocol
+    print(request)
+    if request:
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        client_socket.sendall(response.encode("utf-8"))
+    
+
+
 
 
 if __name__ == "__main__":
