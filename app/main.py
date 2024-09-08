a8877cd721b69a3c6cb4a8b35f0ce241bb986242
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-09-08 18:05:16 +0800

    chore: extract url path

--- app/main.py
+++ app/main.py
@@ -8,17 +8,19 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, client_address =   server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept()  # wait for client
     request = client_socket.recv(1024).decode("utf-8")
-    # request_line = request.split("\r\n") # which is the first line fo the http request
-    # method, path, protocol = request_line[0].split(" ") # split the first line into method, path and protocol
-    print(request)
-    if request:
+    request_line = request.split("\r\n")  # which is the first line fo the http request
+    method, path, protocol = request_line[0].split(
+        " "
+    )  # split the first line into method, path and protocol
+
+    if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
         client_socket.sendall(response.encode("utf-8"))
-    
-
-
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        client_socket.sendall(response.encode("utf-8"))
 
 
 if __name__ == "__main__":
