276ab1b7dd2819230e11412d058df5eb928e02fd
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2024-09-17 18:53:58 +0800

    chore: read header and return header's user agent as response body

--- app/main.py
+++ app/main.py
@@ -12,21 +12,32 @@ def main():
     client_socket, client_address = server_socket.accept()  # wait for client
     request = client_socket.recv(1024).decode("utf-8")
     headers, body = request.split("\r\n\r\n", 1)
-    first_line = headers.split("\r\n")  # which is the first line of the request
-    method, path, protocol = first_line[0].split(
+    first_line, host, accept, user_agent = headers.split(
+        "\r\n"
+    )  # which is the first line of the request in the headers
+
+    method, path, protocol = first_line.split(
         " "
     )  # split the first line into method, path and protocol
-    regex = re.match(r"/echo/(.*+)", path)
+    user_agent = user_agent.split(": ")[1]
+
+    is_echo_route = re.match(r"/echo/(.*+)", path)
+    is_user_agent_route = re.match(r"/user-agent", path)
     if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
         client_socket.sendall(response.encode("utf-8"))
-    elif regex:
+    elif is_echo_route:
         response = (
-            f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(regex.group(1))}\r\n\r\n"
-            + regex.group(1)
+            f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(is_echo_route.group(1))}\r\n\r\n"
+            + is_echo_route.group(1)
+            + "\r\n"
         )
-
         client_socket.sendall(response.encode("utf-8"))
+    elif is_user_agent_route:
+
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}\r\n"
+        client_socket.sendall(response.encode("utf-8"))
+
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
         client_socket.sendall(response.encode("utf-8"))
